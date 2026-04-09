125c95248a93886e5ffeb90f71c7ffa51d939814
spartancmd <strummerdeniel@gmail.com>
2026-04-09 11:48:37 +0200

    change: no need to write the '../' mess in the include path

--- src/looting/Inventory.cpp
+++ src/looting/Inventory.cpp
@@ -1,4 +1,4 @@
-#include "../../include/looting/Inventory.hpp"
+#include "looting/Inventory.hpp"
 
 Inventory::Inventory(sf::Vector2u _inventorySize, const std::string& _title) : title{_title} {
     this->inventorySize = _inventorySize;
--- src/looting/InventorySlot.cpp
+++ src/looting/InventorySlot.cpp
@@ -1,6 +1,6 @@
-#include "../../include/looting/InventorySlot.hpp"
-#include "../../include/core/Window.hpp"
-#include "../../include/gui/InventoryInterface.hpp"
+#include "looting/InventorySlot.hpp"
+#include "core/Window.hpp"
+#include "gui/InventoryInterface.hpp"
 
 InventorySlot::InventorySlot() {
     item = nullptr;
