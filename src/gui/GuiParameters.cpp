6f319905dac99a5d8f0c288a0e09ce817f81383b
MaxAve <maxave909@gmail.com>
2026-05-05 13:29:02 +0200

    Made the inventory background slightly brighter to make items easier to see

--- src/gui/GuiParameters.cpp
+++ src/gui/GuiParameters.cpp
@@ -3,7 +3,7 @@
 namespace GuiParameters {
 
 const sf::Color backgroundColor(20, 20, 25, 150);
-const sf::Color foregroundColor(55, 60, 65, 230);
+const sf::Color foregroundColor(65, 70, 75, 230);
 const float outlineThickness = 2;
 const sf::Color outlineColor(100, 105, 115);
 unsigned slotSizeU = 96u;
