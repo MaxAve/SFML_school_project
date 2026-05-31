b6fce4208bb4222e2bcbcf3eae21ce17c1a4b295
spartancmd <strummerdeniel@gmail.com>
2026-05-31 21:29:08 +0200

    changed button colors slightly

--- src/gui/Button.cpp
+++ src/gui/Button.cpp
@@ -1,10 +1,11 @@
 #include "gui/Button.hpp"
 #include "core/Window.hpp"
+#include "resources/AudioManager.hpp"
 #include "resources/Fonts.hpp"
 
-sf::Color Button::buttonColor = sf::Color::Red;
-sf::Color Button::highlightColor = sf::Color::Yellow;
-sf::Color Button::outlineColor = sf::Color::Black;
+sf::Color Button::buttonColor = sf::Color(112, 112, 112);
+sf::Color Button::highlightColor = sf::Color(140, 140, 140);
+sf::Color Button::outlineColor = sf::Color(0, 0, 0);
 
 Button::Button(sf::Vector2f position, sf::Vector2f size, const std::string& text_, std::function<void()> lambda)
     : text(Fonts::pixel, text_, 30U), action{lambda} {
@@ -30,7 +31,9 @@ bool Button::isHovered() const {
     return hovered;
 }
 
-void Button::setHovered(bool val) { hovered = val; }
+void Button::setHovered(bool val) {
+    hovered = val;
+}
 
 void Button::setPosition(sf::Vector2f pos) {
     shape.setPosition(pos);
@@ -38,6 +41,7 @@ void Button::setPosition(sf::Vector2f pos) {
 }
 
 void Button::run() {
+    AudioManager::playSound("click");
     if (action)
         action();
 }
